b5da8785f4ecaa25c8720e3f15a8f95c14553919
Mark123M <mark123ymm@gmail.com>
2023-10-08 18:33:05 -0400

    proxying with netilify redirects

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = `${process.env.REACT_APP_API_ENDPOINT}api`;
+const baseUrl = `/api`;
 
 export const apiService = {
     get: (url) => axios.get(baseUrl + url),
