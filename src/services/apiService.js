3b13ed7dbdf958ce446eca5c1522ae18ee2ff987
Mark123M <mark123ymm@gmail.com>
2023-10-08 18:55:46 -0400

    proxying with netilify redirects

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -3,10 +3,10 @@ import axios from "axios";
 const baseUrl = `/api`;
 
 export const apiService = {
-    get: (url) => axios.get(baseUrl + url),
-    post: (url, data) => axios.post(baseUrl + url, data),
-    put: (url, data) => axios.put(baseUrl + url, data),
-    delete: (url) => axios.delete(baseUrl + url)
+    get: (url) => axios.get(baseUrl + url + '/'),
+    post: (url, data) => axios.post(baseUrl + url + '/', data),
+    put: (url, data) => axios.put(baseUrl + url + '/', data),
+    delete: (url) => axios.delete(baseUrl + url + '/')
 }
 
 export const getCookie = (key) => {
